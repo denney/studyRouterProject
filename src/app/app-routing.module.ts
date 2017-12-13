014c2caf3e7d1d6c32fc32103bf1905c4d6cefbf
duanshutao <duanshutao@want-want.com>
2017-12-13 22:13:00 +0800

    code404定义及通配符路由配置

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ProductComponent} from './product/product.component';
+import {Code404Component} from './code404/code404.component';
 
 const routes: Routes = [
+  {path: 'product', component: ProductComponent},
   {path: '', component: HomeComponent},
-  {path: 'product', component: ProductComponent}
+  {path: '**', component: Code404Component}
 ];
 
 @NgModule({
